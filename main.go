141ab8d57a60b011aefa996d64f4f65fe71648a5
zhoujing01 <zhoujing01@jsfund.cn>
2023-08-22 17:56:54 +0800

    go routine

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
-import "goapp/packages/custommodule"
+import (
+	"fmt"
+	"time"
+)
+
+// input channel
+func sendNumber(sendCh chan<- int) {
+	fmt.Println("Send Number channel")
+	sendCh <- 4
+}
+
+// output channel
+func getNumber(getCh <-chan int) {
+	num := <- getCh
+	fmt.Printf("Recieved %v", num)
+}
 
 func main() {
-    fmt.Println("Hello, World!")
-		fmt.Println(custommodule.Add(1, 2))
+	ch := make(chan int)
+	go sendNumber(ch)
+	go getNumber(ch)
+	time.Sleep(1 * time.Second)
 }
