5394495aa50ce351e4d718529ff2131a8b613024
zhoujing01 <zhoujing01@jsfund.cn>
2023-08-23 15:14:21 +0800

    mutex

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var counter int = 0
+
+func incr(wg *sync.WaitGroup, mutex *sync.Mutex) {
+	mutex.Lock()
+	counter++
+	mutex.Unlock()
+	wg.Done()
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	for index :=0; index < 1000; index++ {
+		wg.Add(1)
+		go incr(&wg, &mutex)
+	}
+
+	wg.Wait()
+  fmt.Printf("Final value %d", counter)
 }
