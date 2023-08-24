31f4e9c01bba1465c5ba678d2d40b9231d8a663a
zhoujing01 <zhoujing01@jsfund.cn>
2023-08-24 18:51:19 +0800

    configuration with go viper

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
-    "net/http"
     "fmt"
     "goapp/routes"
+    "github.com/spf13/viper"
 )
 
 type User struct {
@@ -15,21 +15,24 @@ type User struct {
 }
 
 func main() {
+    // reading config by viper
+    viper.SetConfigName("config")
+    viper.SetConfigType("yaml")
+    viper.AddConfigPath(".")
+
+    err := viper.ReadInConfig() // find and read the config file
+    if err != nil {
+        panic(fmt.Errorf("Fatal error reading config file"))
+    }
+
     gin.SetMode(gin.ReleaseMode)
     router := gin.Default()
-    router.GET("/api/ping", func (c *gin.Context) {
-        // type H map[string]interface{}
-        c.JSON(http.StatusOK, gin.H{
-            "message": "PONG",
-        })
-    })
-
     apiGroup := router.Group("/api")
 
     routes.SetupUserRoutes(apiGroup)
     routes.SetupBookRoutes(apiGroup)
 
-    port := ":8080"
-    fmt.Printf("You are listening on port %v", port )
+    port := ":" + viper.GetString("server.port")
+    fmt.Printf("You are listening on port %s", port )
     router.Run(port) // default 8080 port
 }
