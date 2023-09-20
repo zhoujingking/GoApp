842ae995d3c87d1ce05d29d9f2bac012edf9a761
zhoujing01 <zhoujing01@jsfund.cn>
2023-09-20 10:15:40 +0800

    pub/sub demo

--- sender.go
+++ sender.go
@@ -22,15 +22,16 @@ func main() {
 	failOnError(err, "failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare("hello", false, false, false, false, nil)
-	failOnError(err, "failed to declare a queue")
+	err = ch.ExchangeDeclare("fanout-demo", "fanout", true, false, false, false, nil)
+	failOnError(err, "failed to declare an exchange")
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	body := "hello world"
 	err = ch.PublishWithContext(ctx,
-		"",
-		q.Name, // routing key
+		"fanout-demo",
+		"", // routing key
 		false,
 		false,
 		amqp.Publishing{
